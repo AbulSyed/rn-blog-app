1db56aa787f392640b1cd4e0096d504201abf649
Abul Syed <syed1234@hotmail.co.uk>
2021-05-06 00:49:31 +0100

    context

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import BlogContext from '../context/BlogContext';
 
 const HomeScreen = () => {
+    const value = useContext(BlogContext);
+    
     return ( 
         <View>
             <Text>Home screen</Text>
+            <Text>{ value }</Text>
         </View>
      );
 }
