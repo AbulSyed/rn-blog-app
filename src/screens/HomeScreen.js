52cc80b7e4d1bd1f3bfbc3718aa8b4734b344efb
Abul Syed <syed1234@hotmail.co.uk>
2021-05-08 02:04:52 +0100

    style

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,13 +1,13 @@
 import React, { useContext } from 'react';
 import { View, Text, StyleSheet, FlatList, Button } from 'react-native';
 import { Context } from '../context/BlogContext';
+import { Feather } from '@expo/vector-icons';
 
 const HomeScreen = () => {
     const { state, addBlog } = useContext(Context);
 
     return ( 
         <View>
-            <Text>Home screen</Text>
             <Button
                 title="add blog"
                 onPress={ () => addBlog() }
@@ -16,7 +16,10 @@ const HomeScreen = () => {
                 data={ state }
                 keyExtractor={ blog => blog.title }
                 renderItem={ ({ item }) => (
-                    <Text>{ item.title }</Text>
+                    <View style={ styles.viewStyle }>
+                        <Text style={ styles.textStyle }>{ item.title }</Text>
+                        <Feather style={ styles.iconStyle } name="trash" />
+                    </View>
                 ) }
             />
         </View>
@@ -24,7 +27,19 @@ const HomeScreen = () => {
 }
 
 const styles = StyleSheet.create({
-
+    viewStyle: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        padding: 10,
+        borderBottomWidth: 1,
+        borderColor: 'gray'
+    },
+    textStyle: {
+        fontSize: 18
+    },
+    iconStyle: {
+        fontSize: 30
+    }
 })
  
 export default HomeScreen;
