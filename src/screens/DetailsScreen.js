a575e8fd0984d607eb031031e9105d89ecd8fdfa
Abul Syed <syed1234@hotmail.co.uk>
2021-05-11 01:22:05 +0100

    navigation.getParam(id)

--- src/screens/DetailsScreen.js
+++ src/screens/DetailsScreen.js
@@ -1,12 +1,16 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import { Context } from '../context/BlogContext';
 
 const DetailsScreen = ({ navigation }) => {
+    const { state } = useContext(Context);
     const id = navigation.getParam('id');
+
+    const blogs = state.find(blog => blog.id === id);
 
     return ( 
         <View>
-            <Text>{ id }</Text>
+            <Text>{ blogs.title }</Text>
         </View>
      );
 }
